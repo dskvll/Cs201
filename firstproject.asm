963850151d3a8bdc929961adf3b11db9bcf468aa
dskvll <44146421+dskvll@users.noreply.github.com>
2018-10-14 23:19:31 -0400

    sets up the requisite framework.
    sets up the framework for creating a mips project

---
+++ firstproject.asm
@@ -0,0 +1,9 @@
+# PROGRAM: first project
+.data # Data declaration section
+
+.text # Assembly language instructions
+main: # Start of code section
+
+syscall # call operating system to perform operation
+
+syscall # Exit program
