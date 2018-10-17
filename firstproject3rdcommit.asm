684779ee89e1736dbee8dcd5524daab7ee6275f3
dskvll <44146421+dskvll@users.noreply.github.com>
2018-10-16 23:54:15 -0400

    Add files via upload

---
+++ firstproject3rdcommit.asm
@@ -0,0 +1,54 @@
+# PROGRAM: first project
+.data # Data declaration section
+
+.text # Assembly language instructions
+main: # Start of code section
+li $v0, 11 # print character instruction
+la $a0, 64 # this is the ascii value for @
+
+syscall# runs the above instructions
+
+li $v0, 1 # print integer instruction
+addi $a0, $a0, -64 # subtracts 64 so the int is 0
+syscall # call operating system to perform operation
+
+addi $a0, $a0, 2 # int = 2
+syscall 
+
+addi $a0, $a0, 6 # int = 8
+syscall
+
+addi $a0, $a0, -4# int = 4
+syscall 
+
+addi $a0, $a0, -1 # int = 3
+syscall 
+
+addi $a0, $a0, 1 # int = 4
+syscall 
+
+addi $a0, $a0, 1 # int = 5
+syscall
+
+ addi $a0, $a0, -5 # int = 0
+syscall 
+
+li $v0, 11 # print character instruction
+add $a0, $a0, 32# this is the ascii value for 'space'
+syscall
+ 
+addi $a0, $a0, 46# this is the ascii value for 'N'
+syscall 
+
+addi $a0, $a0, 27# this is the ascii value for 'i
+syscall 
+
+addi $a0, $a0, 46# this is the ascii value for 'N'
+syscall 
+ 
+
+
+
+ 
+
+
