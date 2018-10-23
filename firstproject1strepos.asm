59285c1e9237f1a1d19b8655063cd6fec99626da
dskvll <44146421+dskvll@users.noreply.github.com>
2018-10-23 14:00:06 -0400

    adds name

---
+++ firstproject1strepos.asm
@@ -0,0 +1,36 @@
+# PROGRAM: first project
+.data # Data declaration section
+
+.text # Assembly language instructions
+main: # Start of code section
+li $v0, 11 # print character instruction
+la $a0, 64 # this is the ascii value for @
+
+syscall# runs the above instructions
+
+li $v0, 1 # print integer instruction
+addi $a0, $a0, -64 # subtracts 64 so the int is 0
+syscall # call operating system to perform operation
+
+addi $a0, $a0, 2 # int = 2
+syscall 
+
+addi $a0, $a0, 6 # int = 8
+syscall
+
+addi $a0, $a0, -4# int = 4
+syscall 
+
+addi $a0, $a0, -1 # int = 3
+syscall 
+
+addi $a0, $a0, 1 # int = 4
+syscall 
+
+ 
+
+
+
+ 
+
+
